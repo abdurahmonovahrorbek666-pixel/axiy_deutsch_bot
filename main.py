f09f5f799a25bd150c540a4504ac98f9e3ea341b
abdurahmonovahrorbek666-pixel <abdurahmonovahrorbek666@gmail.com>
2026-08-24 17:58:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher, html, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
-TOKEN = "8912605806:AAEshu0Br0OVKsT1QXSg43Yy9NBJzOzW2Z8"
+TOKEN = "8912605806:AAEshu0BrOOVKsT1QXSg43Yy9NBJzOZw2Z8"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
