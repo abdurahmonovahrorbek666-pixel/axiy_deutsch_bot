a141653bae0721b5de945a3c691e4f758ee3c69c
abdurahmonovahrorbek666-pixel <abdurahmonovahrorbek666@gmail.com>
2026-08-24 18:04:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import asyncio
-import os
-from aiohttp import web
 from aiogram import Bot, Dispatcher, html, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
-TOKEN = "8912605806:AAEshu0BrOOVKsT1QXSg43Yy9NBJzOZw2Z8"
+TOKEN = "8912605806:AAEshu0Br0OVKsT1QXSg43Yy9NBJzOzW2Z8"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -37,24 +35,7 @@ async def test_handler(message: Message) -> None:
 async def about_handler(message: Message) -> None:
     await message.answer("Ushbu bot Nemis tilini o'rganuvchilar uchun maxsus yaratilgan.")
 
-async def handle_web(request):
-    return web.Response(text="Bot active")
-
 async def main():
-    # Web server yaratish
-    app = web.Application()
-    app.router.add_get("/", handle_web)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    
-    port = int(os.environ.get("PORT", 8080))
-    site = web.TCPSite(runner, "0.0.0.0", port)
-    
-    # Bir vaqtda ham portni tinglash, ham pollingni ishlatish
-    await asyncio.gather(
-        site.start(),
-        dp.start_polling(bot)
-    )
+    await dp.start_polling(bot)
 
-if name == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
